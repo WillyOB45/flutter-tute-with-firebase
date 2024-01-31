e1b1b004a5785806af086a97ed77a1310ccef399
WillyOB45 <chukswilly45@gmail.com>
2024-01-31 08:45:55 +0100

    widgetute

--- lib/pages/widgetute.dart
+++ lib/pages/widgetute.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class widgetute extends StatelessWidget {
-  const widgetute({super.key});
+  final PageController _controller = PageController();
+  widgetute({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [],
-      ),
     );
   }
 }
+
+
+
